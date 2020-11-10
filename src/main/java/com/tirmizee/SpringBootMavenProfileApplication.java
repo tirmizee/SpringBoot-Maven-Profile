193d1e5449135a5891722536216c5fd926128e7c
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-11-10 11:17:46 +0700

    init project

---
+++ src/main/java/com/tirmizee/SpringBootMavenProfileApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenProfileApplication.class, args);
+	}
+
+}
