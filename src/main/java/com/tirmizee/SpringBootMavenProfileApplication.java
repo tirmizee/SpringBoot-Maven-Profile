17752fd3d5575d86d1ad92f9f2a9def1090c3297
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-11-10 13:50:21 +0700

    add profile properties

--- src/main/java/com/tirmizee/SpringBootMavenProfileApplication.java
+++ src/main/java/com/tirmizee/SpringBootMavenProfileApplication.java
@@ -1,13 +1,33 @@
 package com.tirmizee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class SpringBootMavenProfileApplication {
-
+public class SpringBootMavenProfileApplication implements CommandLineRunner {
+	
+	@Autowired 
+	ApplicationContext context;
+	
+	@Value("${pom.description}") 	
+	private String description;
+	
+	@Value("${pom.test}") 	
+	private String test;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMavenProfileApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(test);
+		System.out.println(description);
 	} 
 
 }
